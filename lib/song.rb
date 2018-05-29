851f1cc60fd9fd92595c433048e65d5da387e8ea
Ayana Bando <ayanaflax@yahoo.com>
2018-05-29 18:00:15 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -18,7 +18,7 @@ class Song
   def self.new_by_filename(filename)
     song = self.new(filename.split(" - ")[1])
     song.artist_name = filename.split(" - ")[0]
-    binding.pry
     song
+    binding.pry
   end
 end
